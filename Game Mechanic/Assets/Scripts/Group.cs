3385283d98fd075b84b26d706e213da50386ea05
Cambrai1 <32384758+Cambrai1@users.noreply.github.com>
2017-10-06 14:57:28 +0100

    Initial Project 1st Commit.
    Given the Player object the ability to jump and move around smoothly. Very Basic Movement right now.

---
+++ Game Mechanic/Assets/Scripts/Group.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class Group : MonoBehaviour
+{
+    private CanvasGroup m_group;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_group = GetComponent<CanvasGroup>();
+
+        m_group.alpha = 0;
+    }
+
+    [SerializeField]
+    private AudioMixer m_mixer;
+
+    [SerializeField]
+    private SoundManager m_SoundManager;
+
+    public void SetAmbientVolume(float _volume)
+    {
+        m_mixer.SetFloat("AmbientVolume", _volume);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(m_group.alpha == 0)
+            {
+                m_group.alpha = 1;
+                m_SoundManager.Menumode();
+            }
+            else
+            {
+                m_group.alpha = 0;
+                m_SoundManager.Gamemode();
+            }
+        }
+	}
+}
---
+++ Game Mechanic/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour {
+
+    [SerializeField]
+    private AudioMixerSnapshot m_Gamemode;
+
+    [SerializeField]
+    private AudioMixerSnapshot m_Menumode;
+
+    public void Menumode()
+    {
+        m_Menumode.TransitionTo(0.5f);
+    }
+
+    public void Gamemode()
+    {
+        m_Gamemode.TransitionTo(0.5f);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
