831dccf2193a9155f2f654440dfff6d0d42f6e04
Cambrai1 <32384758+Cambrai1@users.noreply.github.com>
2017-10-10 15:03:26 +0100

    Changed A-D from rotation to position
    Added Power Slider and max+min jumps

--- Game Mechanic/Assets/Scripts/CharBehaviour.cs
+++ Game Mechanic/Assets/Scripts/CharBehaviour.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 namespace ISS
@@ -11,13 +12,20 @@ namespace ISS
         [SerializeField]
         private float m_rotationSpeed = 50.0f;
 
+        [SerializeField]
+        private Slider slider;
+
+        [SerializeField]
+        private Image image;
+
         private Transform m_transform;
         public Rigidbody rb;
         public float speed = 10.0f;
         public int moveSpeed = 10;
         public float downTime, upTime, power;
-
-
+        public float minJump = 10;
+        public float maxJump = 20;
+        
         Vector3 down = new Vector3(0, -1, 0);
 
         void Start()
@@ -29,16 +37,16 @@ namespace ISS
         // Update is called once per frame
         void Update()
         {
-            //if (rb.velocity.y < -2 && grounded == false)
-            //{
-            //    grounded = true;
-            //}
 
+            
+
+            image.enabled = !(slider.value == 10);
 
             float rotY = m_transform.rotation.eulerAngles.y;
 
-            m_transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotY + m_rotationSpeed *
-                Input.GetAxis("Horizontal") * Time.deltaTime, 0.0f));
+            //m_transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotY + m_rotationSpeed *
+            //Input.GetAxis("Horizontal") * Time.deltaTime, 0.0f));
+            m_transform.position += m_transform.right * m_speed * Input.GetAxis("Horizontal") * Time.deltaTime;
 
             m_transform.position += m_transform.forward * m_speed * Input.GetAxis("Vertical") * Time.deltaTime;
 
@@ -47,26 +55,27 @@ namespace ISS
                 downTime = Time.time;
             }
 
-
-
-            if (Input.GetKeyUp("space"))
+            if (Input.GetKey("space"))
             {
                 upTime = Time.time;
-                power = ((upTime - downTime) * 6);
-
-                Debug.DrawRay(transform.position, down, Color.red, 5);
-                if (Physics.Raycast(transform.position, down, 2))
-                {
-
-                    rb.velocity = new Vector3(rb.velocity.x, power, rb.velocity.z);
-
-                    Debug.Log(Time.time);
-
-                }
-
+                power = (((upTime - downTime) * 10) + minJump);
+                slider.value = power;
             }
 
-            
+           
+            if (Input.GetKeyUp("space"))
+            {             
+                Debug.DrawRay(transform.position, down, Color.red, 5);
+
+                if (Physics.Raycast(transform.position, down, 2))
+                {
+                    rb.velocity = new Vector3(rb.velocity.x, (power > maxJump) ? maxJump : power, rb.velocity.z);
+
+                    Debug.Log(power);
+
+                    slider.value = 10;
+                }
+            }
         }
     }
 }       
