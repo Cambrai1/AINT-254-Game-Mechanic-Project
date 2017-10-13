dbdf6b606b0b79660515096d2926a938fc12ad87
Cambrai1 <32384758+Cambrai1@users.noreply.github.com>
2017-10-13 15:08:12 +0100

    Not perfect, tried to change something but needs fixing

---
+++ Game Mechanic/Assets/Scripts/CamBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ISS
+{
+    public class CamBehaviour : MonoBehaviour
+    {
+
+        [SerializeField]
+        private float m_speed = 10.0f;
+
+        // Use this for initialization
+        void Start()
+        {
+            m_transform = GetComponent<Transform>();
+            rb = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (Input.GetKey("up"))
+            {
+                m_transform.position -= m_transform.forward * m_speed * Time.deltaTime;
+            }
+        }
+    }
+}
+
