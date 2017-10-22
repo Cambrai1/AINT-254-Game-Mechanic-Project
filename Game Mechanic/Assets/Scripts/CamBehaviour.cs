b1d30d157f26e30de109bf148a51715aad51c4c3
Cambrai1 <32384758+Cambrai1@users.noreply.github.com>
2017-10-21 19:35:06 -0600

    Added Bounce to Walls and Spheres
    ^^ in title, problem with camera falling through ground after scene reset.

--- Game Mechanic/Assets/Scripts/CamBehaviour.cs
+++ Game Mechanic/Assets/Scripts/CamBehaviour.cs
@@ -30,22 +30,11 @@ namespace ISS
         void Update()
         {
             float rotY = m_player.rotation.eulerAngles.y;
-            //Debug.Log(m_transform.localPosition.z);
+           
 
             var distance = Vector3.Distance(m_transform.position, m_player.position);
             Debug.Log(distance);
-
-            //if (Input.GetKey("down") && (distance < 5))
-            //{
-              //  m_transform.localPosition -= m_transform.forward * m_speed * Time.deltaTime;
-                //distance += m_transform.forward * m_speed * Time.deltaTime;
-            //}
-
-            //if (Input.GetKey("up") && (distance > 1))
-            //{
-              //  m_transform.localPosition += m_transform.forward * m_speed * Time.deltaTime;
-            //}
-
+            
             if (Input.GetKey("left"))
             {
                 m_player.rotation = Quaternion.Euler(new Vector3(0.0f, rotY - m_rotationSpeed * Time.deltaTime, 0.0f));
