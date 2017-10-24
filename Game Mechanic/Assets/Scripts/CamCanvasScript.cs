95d20ce62974dc02e2db208452f4ba462dc7a16c
Cambrai1 <32384758+Cambrai1@users.noreply.github.com>
2017-10-24 15:06:11 +0100

    Added a high score and current score file to game

---
+++ Game Mechanic/Assets/Scripts/CamCanvasScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CamCanvasScript : MonoBehaviour {
+
+    public int currentHeight;
+    public int bestHeight;
+	// Use this for initialization
+	void Start () {
+        currentHeight = 10;
+        bestHeight = 10;
+	}
+    [SerializeField]
+    private Text m_text;
+	void Update () {
+
+        if (currentHeight >= bestHeight)
+        {
+            bestHeight = currentHeight;
+
+        }
+
+       m_text.text = ("Best Height:" + bestHeight.ToString() + "\nCurrent Height:" + currentHeight.ToString());
+    }
+}
