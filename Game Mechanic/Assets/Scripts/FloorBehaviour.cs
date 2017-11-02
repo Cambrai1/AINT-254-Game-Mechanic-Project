85c15423a98c3be0e99c07c1339b550ed0d99911
Cambrai1 <32384758+Cambrai1@users.noreply.github.com>
2017-11-02 13:50:20 -0600

    added more spawn points for the spheres and changed the ambient music

--- Game Mechanic/Assets/Scripts/FloorBehaviour.cs
+++ Game Mechanic/Assets/Scripts/FloorBehaviour.cs
@@ -2,34 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FloorBehaviour : MonoBehaviour {
-
-    public GameObject SpherePre;
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-    }
-
-    void OnCollisionEnter(Collision Sphere)
+namespace ISS
+{
+    public class FloorBehaviour : MonoBehaviour
     {
-        if (Sphere.gameObject.tag == "Sphere")
+
+        public GameObject SpherePre;
+
+        // Use this for initialization
+        void Start()
         {
-            Destroy(Sphere.gameObject);
-            SpawnSphere();
-            //Instantiate(Sphere, 0.0.0 , Quaternion.identity);
+
         }
 
-    }
+        // Update is called once per frame
+        void Update()
+        {
 
-    private void SpawnSphere()
-    {
-        Vector3 newPos = new Vector3(Random.Range(-26, 29), 100, Random.Range(-28, 60));
-        GameObject octo = Instantiate(SpherePre, newPos, Quaternion.identity) as GameObject;
+        }
+
+        void OnCollisionEnter(Collision Sphere)
+        {
+            if (Sphere.gameObject.tag == "Sphere")
+            {
+                Destroy(Sphere.gameObject);
+                SpawnSphere();
+                //Instantiate(Sphere, 0.0.0 , Quaternion.identity);
+            }
+
+        }
+
+        private void SpawnSphere()
+        {
+            if (GameObject.Find("Character").GetComponent<CharBehaviour>().heightY < 30)
+            {
+                Vector3 newPos = new Vector3(Random.Range(-26, 29), 100, Random.Range(-28, 60));
+                GameObject octo = Instantiate(SpherePre, newPos, Quaternion.identity) as GameObject;
+            }
+            else if (GameObject.Find("Character").GetComponent<CharBehaviour>().heightY >= 30)
+            {
+                Vector3 newPos = new Vector3(Random.Range(-130, 29), 125, Random.Range(-59, -37));
+                GameObject octo = Instantiate(SpherePre, newPos, Quaternion.identity) as GameObject;
+            }
+        }
     }
 }
+
